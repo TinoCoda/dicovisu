e458199cb5e00e0f53208ec899cccc152f34e77d
Destin B.Moanda <moanda@live.fr>
2025-03-01 18:13:17 +0100

    setup the database and test the connection

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running on port ${PORT}`)
+    
+});
