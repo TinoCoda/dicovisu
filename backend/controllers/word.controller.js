eb22a2d500b35a068351a1555190ccdd250cccf6
Destin B.Moanda <moanda@live.fr>
2025-03-01 19:14:56 +0100

    set the controllers for word

---
+++ backend/controllers/word.controller.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import Word from "../models/word.model.js";
+
+export const getWords = async (req, res) => {
+    try {
+        const words = await Word.find();
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json({ succes: false,message: error.message });
+    }
+};
+
+export const createWord = async (req, res) => {
+    const word = req.body;
+    if(!word.word || !word.meaning || !word.language){
+        res.status(400).json({ succes:false,message: "Please fill all required fields" });
+    }
+    const newWord = new Word(word);
+    try {
+        await newWord.save();
+        res.status(201).json(newWord);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+};
+
+export const updateWord = async (req, res) => {
+    const { id: _id } = req.params;
+    const word = req.body;
+    if(!mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).json({ sucess:true,message: "No word with that id" });
+    }
+    const updatedWord = await Word.findByIdAndUpdate(id,word, {new: true});
+    res.json(updatedWord);
+
+};
+
+export const deleteWord = async (req, res) => {
+    try{
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({ sucess:false,message: "No word with that id" });
+        }
+        await Word.findByIdAndRemove(id);
+        res.status(200).json({ success: true, message: "Word deleted successfully" });
+
+    }catch(error){
+        console.error(`Error while deleting word: ${error.message}`);
+        res.status(500).json({ success: false, message: error.message });
+
+    }
+
+};
+
+export const searchWordStart = async (req, res) => {
+    const { word } = req.query;
+    try {
+        const words = await Word.find({ word: { $regex: `^${word}`, $options: "i" } });
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+};
+
