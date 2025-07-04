7f07410408fad7c6ad194c6341bdce7c1e70597f
Destin B.Moanda Office <moanda@live.fr>
2025-07-04 09:11:41 +0200

    change base url

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { baseStore } from '../../store/global';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = '/api'; // früher war's 'http://localhost:5000/api'
 
 
 
